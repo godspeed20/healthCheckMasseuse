697885581e64b8c378cb4b37ee385d192a9ac767
godspeed20 <rzkd@hotmail.com>
2017-01-10 21:46:29 +0800

    single layer json

--- server.py
+++ server.py
@@ -1,26 +1,27 @@
+import json
+
+from urllib.request import urlopen
+
 from flask import Flask
 from flask import jsonify
-import json
-from urllib import request
+from flask import request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    link = "http://localhost:5000" #get this from the input param
 
-    f = request.urlopen(link)
-    jsonString = f.read()
-    jsonData = json.loads(jsonString)
+@app.route("/single-layer")
+def single_layer():
+    health_check_url = request.args.get('healthCheckUrl')
+    app_name = request.args.get('appName')
+    if health_check_url is None or app_name is None: return "Params healthCheckUrl and appName are mandatory", 400
 
-    components = []
-    print(jsonData)
-    print(jsonData.items())
-    for key, value in jsonData.items():
-        components.append({"name": key, "status": value})
-    print(components)
+    f = urlopen(health_check_url)
+    json_data = json.loads(f.read().decode("utf-8"))
 
-    return  jsonify({"components": components})
+    components = [{"name": key, "status": value} for key, value in json_data.items()]
+
+    return jsonify({"name": app_name, "components": components})
+
 
 if __name__ == "__main__":
     app.run(port=5001)
